de5c3c87640ad4cfb4de7a4935451dbda1614947
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-12-29 19:45:43 +0200

    Added urls

--- fruitipedia/urls.py
+++ fruitipedia/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("fruitipedia.common.urls")),
+    path('profile/', include('fruitipedia.profiles.urls')),
+    path('fruits/', include('fruitipedia.fruits.urls')),
+
 ]
